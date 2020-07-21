30cb3dd62141820bc356c9fbe504ed309cce9e2f
nscoincommunity <64058715+nscoincommunity@users.noreply.github.com>
2020-07-21 13:25:51 +0700

    Update index.php

--- index.php
+++ index.php
@@ -4,4 +4,7 @@ include("./easybitcoin.php");
   $bitcoin = new Bitcoin($rpc_user,$rpc_pass,$rpc_host); 	
   $saldodoge = $bitcoin->getbalance($user_session);
 
+$addr=print_r($bitcoin->getnewaddress (),true); echo "\n";
+echo "addr=".$addr; echo "\n";
+
 ?>
